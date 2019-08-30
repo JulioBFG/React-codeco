4b86fc6cfa0ce22f87ec244d27208908cdc1a12c
Your Name <juliolopes.bfg@gmail.com>
2019-08-30 18:57:01 -0300

    Cacete

--- my-app/src/reducer.js
+++ my-app/src/reducer.js
@@ -5,6 +5,7 @@ const INITIAL_STATE = {
     tweets: []
 }
 const reducer = (state = INITIAL_STATE, action)  => {
+    // eslint-disable-next-line default-case
     switch (action.type){
         case 'TWEETS_ADD':
             return { 
